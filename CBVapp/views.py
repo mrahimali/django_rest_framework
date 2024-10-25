ac2fc5aaa4b46982036e342c63ba71280bccce21
Rahim <mrahimali>
2024-10-25 12:00:14 +0530

    implemented mixins and generics

--- CBVapp/views.py
+++ CBVapp/views.py
@@ -4,8 +4,61 @@ from rest_framework.response import Response
 from .models import CourseSerializer, Course
 from rest_framework import status
 from django.http import Http404
+from rest_framework import mixins, generics
+
+
 # Create your views here.
 
+
+
+# without primary key
+class CourseListView(generics.ListCreateAPIView):
+    queryset=Course.objects.all()
+    serializer_class= CourseSerializer
+
+
+
+# Primary key based operations
+class CourseDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Course.objects.all()
+    serializer_class=CourseSerializer
+
+'''
+
+class CourseListView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset=Course.objects.all()
+    serializer_class= CourseSerializer
+    def get(self, request):
+        return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+
+'''
+
+
+'''
+
+class CourseDetailsView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset=Course.objects.all()
+    serializer_class=CourseSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+    
+    def put(self, request, pk):
+        return self.update(request, pk)
+    
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+'''
+
+
+
+
+
+'''
 class CourseListView(APIView):
     def get(self, request):
         courses=Course.objects.all()
@@ -19,8 +72,9 @@ class CourseListView(APIView):
             return Response({'message':'Record Added', 'data':serializer.data})
         else:
             return Response(serializer.errors)
-        
+'''        
 
+'''
 class CourseDetailsView(APIView):
     def get_course(self, pk):
         try:
@@ -49,5 +103,4 @@ class CourseDetailsView(APIView):
             return Response({'message':'Updated', 'data':serializer.data})
         else:
             return Response(serializer.errors)
-    
-    
+'''
